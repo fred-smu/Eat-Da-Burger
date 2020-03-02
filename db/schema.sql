c10fc86e7fd029368c9f53cca9fb3e80001672b1
Fred Guthrie <fred3@myself.com>
2020-03-02 09:45:35 -0600

    good code

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE database burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NULL,
+  devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
